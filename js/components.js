782265fa0bf253510019b0b1c09efe50be4735a6
Tomas Canal <tomascanalsaravia3@gmail.com>
2025-01-21 19:48:22 -0300

    add header design

--- js/components.js
+++ js/components.js
@@ -1,6 +1,7 @@
 export function loadNav() {
   return `
     <ul>
+      <li><img class="favicon" src="./assets/images/favicon.png" alt="favicon"></li>
       <li><a href="index.html">Home</a></li>
       <li><a href="about.html">About Us</a></li>
       <li><a href="adopt.html">Adopt A Pet</a></li>
