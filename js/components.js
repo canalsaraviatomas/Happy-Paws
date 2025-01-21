cca557d824b7da60132a5566517c32c84bad30f1
Tomas Canal <tomascanalsaravia3@gmail.com>
2025-01-21 18:17:29 -0300

    add some initial content

---
+++ js/components.js
@@ -0,0 +1,16 @@
+export function loadNav() {
+  return `
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="about.html">About Us</a></li>
+      <li><a href="adopt.html">Adopt A Pet</a></li>
+      <li><a href="contact.html">Contact</a></li>
+    </ul>
+    `;
+}
+
+export function loadFooter() {
+  return `
+    <p>&copy; 2025 Happy Paws. All Rights Reserved.</p>
+  `;
+}
