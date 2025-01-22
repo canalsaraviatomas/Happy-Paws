25518d9e46bc593108a220189bd83205e96a56f5
Tomas Canal <tomascanalsaravia3@gmail.com>
2025-01-22 11:40:18 -0300

    add Adopt Pet

--- js/components.js
+++ js/components.js
@@ -1,3 +1,11 @@
+export function loadPet() {
+  return `
+    <img src="./assets/images/dog-1.jpg" alt="pet" />
+    <p class="name-pet">Name</p>
+    <p> 2 year old <br> Male </p>
+    `;
+}
+
 export function loadNav() {
   return `
     <ul>
