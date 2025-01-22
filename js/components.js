60b2bd7aec3c8707980a62f5fcf653db2664360d
Tomas Canal <tomascanalsaravia3@gmail.com>
2025-01-21 21:15:44 -0300

    add about us

--- js/components.js
+++ js/components.js
@@ -2,10 +2,10 @@ export function loadNav() {
   return `
     <ul>
       <li><img class="favicon" src="./assets/images/favicon.png" alt="favicon"></li>
-      <li><a href="index.html">Home</a></li>
-      <li><a href="about.html">About Us</a></li>
-      <li><a href="adopt.html">Adopt A Pet</a></li>
-      <li><a href="contact.html">Contact</a></li>
+      <li><a href="./index.html">Home</a></li>
+      <li><a href="./about.html">About Us</a></li>
+      <li><a href="./adopt.html">Adopt A Pet</a></li>
+      <li><a href="./contact.html">Contact</a></li>
     </ul>
     `;
 }
