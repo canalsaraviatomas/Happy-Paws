75493be3ccda4797114b512917706e32bcff672c
Tomas Canal <tomascanalsaravia3@gmail.com>
2025-01-22 15:41:56 -0300

    add modals

--- js/main.js
+++ js/main.js
@@ -6,11 +6,7 @@ function loadPet() {
       const animales = data.data; // array completo de animales
       const randomIndex = Math.floor(Math.random() * animales.length);
       const animalRandom = animales[randomIndex];
-      return `
-        <img src="${animalRandom.imagen}" alt="pet" />
-        <p class="name-pet">${animalRandom.nombre}</p>
-        <p> ${animalRandom.edad} <br> ${animalRandom.genero} </p>
-      `;
+      return animalRandom;
     })
     .catch((error) => {
       console.error("Error al obtener datos de la API:", error);
@@ -22,7 +18,38 @@ export function loadPetsIntoDOM() {
   const petElements = document.querySelectorAll(".pet");
   petElements.forEach((petElement) => {
     loadPet().then((petHTML) => {
-      petElement.innerHTML = petHTML;
+      petElement.innerHTML = `
+        <img src="${petHTML.imagen}" alt="pet" />
+        <p class="name-pet">${petHTML.nombre}</p>
+        <p> ${petHTML.edad} <br> ${petHTML.genero} </p>
+      `;
+      petElement.addEventListener("click", () => {
+        const modal = document.createElement("div");
+        modal.classList.add("modal");
+        modal.innerHTML = `
+          <div class="modal-content">
+            <img src="${petHTML.imagen}" alt="pet" />
+            <div class="text-content">
+              <span class="close-button">&times;</span>
+              <p class="name-pet">${petHTML.nombre}</p>
+              <p class="details">${petHTML.genero} | ${petHTML.edad}</p>
+              <p> ${petHTML.desc_personalidad} ${petHTML.desc_adicional} </p>
+            </div>
+          </div>
+        `;
+        document.body.appendChild(modal);
+
+        const closeButton = modal.querySelector(".close-button");
+        closeButton.addEventListener("click", () => {
+          document.body.removeChild(modal);
+        });
+
+        modal.addEventListener("click", (event) => {
+          if (event.target === modal) {
+            document.body.removeChild(modal);
+          }
+        });
+      });
     });
   });
 }
